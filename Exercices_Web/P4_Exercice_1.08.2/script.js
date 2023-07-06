d3f9836828deb9783d61adae17f95c21d084d569
Sylvain <sylvain.vansteenkiste59@gmail.com>
2023-07-06 16:39:16 +0200

    create function for last exercice

--- Exercices_Web/P4_Exercice_1.08.2/script.js
+++ Exercices_Web/P4_Exercice_1.08.2/script.js
@@ -1,42 +1,46 @@
-const formGlobal = document.createElement('form')
-formGlobal.setAttribute('action', "GET")
-formGlobal.setAttribute('id', "my-form")
+function createForm(){
+    const formGlobal = document.createElement('form')
+    formGlobal.setAttribute('action', "GET")
+    formGlobal.setAttribute('id', "my-form")
+    
+    
+    const inputText = document.createElement('input')
+    inputText.setAttribute('type', 'text')
+    inputText.setAttribute('name', 'name')
+    inputText.setAttribute('id', 'name')
+    inputText.setAttribute('placeholder', 'Username')
+    
+    
+    const inputMdp = document.createElement('input')
+    inputMdp.setAttribute('type', 'password')
+    inputMdp.setAttribute('name', 'pwd')
+    inputMdp.setAttribute('id', 'pwd')
+    inputMdp.setAttribute('placeholder', 'Password')
+    
+    
+    const inputBirthDate = document.createElement('select')
+    inputBirthDate.setAttribute('name', 'birthdate')
+    inputBirthDate.setAttribute('id', 'birth-date-select')
+    
+    
+    const inputSubmit = document.createElement('input')
+    inputSubmit.setAttribute('type', 'submit')
+    inputSubmit.setAttribute('value', 'valider')
+    inputSubmit.setAttribute('id', 'submit-btn')
+    
+    
+    for (var annee = 1980; annee <= 2023; annee++) {
+        var option = document.createElement("option");
+        option.value = annee;
+        option.text = annee;
+        inputBirthDate.appendChild(option);
+      }
+    
+    document.body.appendChild(formGlobal)
+    formGlobal.append(inputText, inputMdp, inputBirthDate,inputSubmit)
+}
 
-
-const inputText = document.createElement('input')
-inputText.setAttribute('type', 'text')
-inputText.setAttribute('name', 'name')
-inputText.setAttribute('id', 'name')
-inputText.setAttribute('placeholder', 'Username')
-
-
-const inputMdp = document.createElement('input')
-inputMdp.setAttribute('type', 'password')
-inputMdp.setAttribute('name', 'pwd')
-inputMdp.setAttribute('id', 'pwd')
-inputMdp.setAttribute('placeholder', 'Password')
-
-
-const inputBirthDate = document.createElement('select')
-inputBirthDate.setAttribute('name', 'birthdate')
-inputBirthDate.setAttribute('id', 'birth-date-select')
-
-
-const inputSubmit = document.createElement('input')
-inputSubmit.setAttribute('type', 'submit')
-inputSubmit.setAttribute('value', 'valider')
-inputSubmit.setAttribute('id', 'submit-btn')
-
-
-for (var annee = 1980; annee <= 2023; annee++) {
-    var option = document.createElement("option");
-    option.value = annee;
-    option.text = annee;
-    inputBirthDate.appendChild(option);
-  }
-
-document.body.appendChild(formGlobal)
-formGlobal.append(inputText, inputMdp, inputBirthDate,inputSubmit)
+createForm()
 
 const inputs = document.getElementById("my-form").elements;
 const loginForm = document.getElementById("my-form")
