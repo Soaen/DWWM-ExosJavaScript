aa4edc2bfabe3405c2ab2e02adb0c0d6680eec51
Soaen <sylvain.vansteenkiste59@gmail.com>
2023-07-11 12:25:40 +0200

    First Exercice finished

--- Exercices_Web/P6_Exercice_1.01/script.js
+++ Exercices_Web/P6_Exercice_1.01/script.js
@@ -7,7 +7,6 @@ async function loadCategories() {
     fetch(linkCateApi)
     .then(response => response.json())
     .then(datas => {
-
         for (let i = 0; i < datas.count; i++) {
             let selectOnHtml = document.getElementById('filter')
 
@@ -17,62 +16,56 @@ async function loadCategories() {
 
             selectOnHtml.appendChild(optionsCreation)
 
-
         }
-
     })
 }
 
-
-
-async function loadEntries() {
+async function loadEntries(filter) {
     fetch(linkEntriesApi)
     .then(response => response.json())
     .then(datas => {
 
         for (let i = 0; i < datas.count; i++) {
-            let selectNameOnHtml = document.getElementById('apiList')
+        
+            if(datas.entries[i].Category === filter){
+                let selectNameOnHtml = document.getElementById('apiList')
 
+                let createTr = document.createElement('tr')
+    
+                let createNameTh = document.createElement('th')
+    
+                createNameTh.textContent = datas.entries[i].API
+    
+                let createDescTh = document.createElement('th')
+    
+                createDescTh.textContent = datas.entries[i].Description
+    
+                let createAuthTh = document.createElement('th')
+    
+                createAuthTh.textContent = datas.entries[i].Auth
+    
+                let createHTTPSTh = document.createElement('th')
+    
+                createHTTPSTh.textContent = datas.entries[i].HTTPS
+    
+                let createCorsTh = document.createElement('th')
+    
+                createCorsTh.textContent = datas.entries[i].Cors
+    
+                let createLinkTh = document.createElement('th')
+    
+                createLinkTh.textContent = datas.entries[i].Link
+    
+                let createCategoryTh = document.createElement('th')
+    
+                createCategoryTh.textContent = datas.entries[i].Category
+    
+                createTr.append(createNameTh, createDescTh, createAuthTh, createHTTPSTh, createCorsTh, createLinkTh, createCategoryTh)
+    
+                selectNameOnHtml.appendChild(createTr)
+            }
 
-            let createTr = document.createElement('tr')
-
-
-
-            let createNameTh = document.createElement('th')
-
-            createNameTh.textContent = datas.entries[i].API
-
-
-            let createDescTh = document.createElement('th')
-
-            createDescTh.textContent = datas.entries[i].Description
-
-            let createAuthTh = document.createElement('th')
-
-            createAuthTh.textContent = datas.entries[i].Auth
-
-            let createHTTPSTh = document.createElement('th')
-
-            createHTTPSTh.textContent = datas.entries[i].HTTPS
-
-            let createCorsTh = document.createElement('th')
-
-            createCorsTh.textContent = datas.entries[i].Cors
-
-            let createLinkTh = document.createElement('th')
-
-            createLinkTh.textContent = datas.entries[i].Link
-
-            let createCategoryTh = document.createElement('th')
-
-            createCategoryTh.textContent = datas.entries[i].Category
-
-            createTr.append(createNameTh, createDescTh, createAuthTh, createHTTPSTh, createCorsTh, createLinkTh, createCategoryTh)
-
-            selectNameOnHtml.appendChild(createTr)
             
-
-
         }
 
     })
@@ -80,4 +73,14 @@ async function loadEntries() {
 
 
 loadCategories()
-loadEntries()
+
+document.getElementById('filter').addEventListener('change', function() {
+    let tempDatasScreen = document.getElementById('apiList')
+    while(tempDatasScreen.hasChildNodes()){
+        tempDatasScreen.removeChild(tempDatasScreen.firstChild)
+    }
+
+    let filter = document.getElementById('filter')
+    let filterValue = filter.value
+    loadEntries(filterValue)
+});
