89c71187b58b3c780552f56daefb15826a470666
Soaen <sylvain.vansteenkiste59@gmail.com>
2023-07-11 12:02:14 +0200

    Fetch done

---
+++ Exercices_Web/P6_Exercice_1.01/script.js
@@ -0,0 +1,83 @@
+const linkApi = 'https://api.publicapis.org/'
+const linkCateApi = 'https://api.publicapis.org/categories'
+const linkEntriesApi = 'https://api.publicapis.org/entries'
+
+
+async function loadCategories() {
+    fetch(linkCateApi)
+    .then(response => response.json())
+    .then(datas => {
+
+        for (let i = 0; i < datas.count; i++) {
+            let selectOnHtml = document.getElementById('filter')
+
+            let optionsCreation = document.createElement('option')
+            optionsCreation.setAttribute('valute', datas.categories[i])
+            optionsCreation.textContent = datas.categories[i]
+
+            selectOnHtml.appendChild(optionsCreation)
+
+
+        }
+
+    })
+}
+
+
+
+async function loadEntries() {
+    fetch(linkEntriesApi)
+    .then(response => response.json())
+    .then(datas => {
+
+        for (let i = 0; i < datas.count; i++) {
+            let selectNameOnHtml = document.getElementById('apiList')
+
+
+            let createTr = document.createElement('tr')
+
+
+
+            let createNameTh = document.createElement('th')
+
+            createNameTh.textContent = datas.entries[i].API
+
+
+            let createDescTh = document.createElement('th')
+
+            createDescTh.textContent = datas.entries[i].Description
+
+            let createAuthTh = document.createElement('th')
+
+            createAuthTh.textContent = datas.entries[i].Auth
+
+            let createHTTPSTh = document.createElement('th')
+
+            createHTTPSTh.textContent = datas.entries[i].HTTPS
+
+            let createCorsTh = document.createElement('th')
+
+            createCorsTh.textContent = datas.entries[i].Cors
+
+            let createLinkTh = document.createElement('th')
+
+            createLinkTh.textContent = datas.entries[i].Link
+
+            let createCategoryTh = document.createElement('th')
+
+            createCategoryTh.textContent = datas.entries[i].Category
+
+            createTr.append(createNameTh, createDescTh, createAuthTh, createHTTPSTh, createCorsTh, createLinkTh, createCategoryTh)
+
+            selectNameOnHtml.appendChild(createTr)
+            
+
+
+        }
+
+    })
+}
+
+
+loadCategories()
+loadEntries()
