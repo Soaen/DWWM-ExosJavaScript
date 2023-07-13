7d85c53c172e50868d63a363fc9eb83bee97801d
Soaen <sylvain.vansteenkiste59@gmail.com>
2023-07-13 11:28:53 +0200

    Messagerie done

--- Exercices_Web/P6_Exercice_1.03/script.js
+++ Exercices_Web/P6_Exercice_1.03/script.js
@@ -2,9 +2,8 @@ const linkMsgApi = 'https://data.snx.ovh/messages.php'
 
 
 async function loadMessages() {
-    fetch(linkMsgApi)
-    .then(response => response.json())
-    .then(datas => {
+    
+    
 
         let containerHtml = document.getElementById('container')
             containerHtml.style.width = '50%'
@@ -19,22 +18,23 @@ async function loadMessages() {
             containerHtml.style.display = 'flex'
             containerHtml.style.flexDirection = 'column-reverse'
 
-
         let formChat = document.createElement('form')
             formChat.style.margin = 'auto';
             formChat.style.marginTop = '20px';
             formChat.style.width = '50%'
 
-
-
         let formSendMessage = document.createElement('form')
+            formSendMessage.setAttribute('id', 'form-send')
             formSendMessage.style.margin = 'auto';
             formSendMessage.style.marginTop = '20px';
             formSendMessage.style.width = '50%'
 
+        
+            fetch(linkMsgApi)
+                .then(response => response.json())
+                .then(datas => {
 
         for (let i = 0; i < datas.length; i++) {
-            
             let contentArticle = document.createElement('article')
             contentArticle.style.marginLeft = '10px'
 
@@ -67,6 +67,7 @@ async function loadMessages() {
         }
 
         let formUsernameAsk = document.createElement('input')
+            formUsernameAsk.setAttribute('id', 'username-input')
             formUsernameAsk.style.width = '100%'
             formUsernameAsk.style.height = '50px'
             formUsernameAsk.style.border = '1px solid lightgrey'
@@ -80,6 +81,8 @@ async function loadMessages() {
             formMessageDiv.style.width = '100%'
 
         let formMessageAsk = document.createElement('input')
+            formMessageAsk.setAttribute('id', 'message-input')
+
             formMessageAsk.style.width = '70%'
             formMessageAsk.style.height = '50px'
             formMessageAsk.style.border = '1px solid lightgrey'
@@ -108,26 +111,73 @@ async function loadMessages() {
 
             containerHtml.after(formSendMessage)
 
+            formSendMessage.addEventListener('submit', function(event) {
+                event.preventDefault(); // Empêche le rechargement de la page lors de la soumission du formulaire
+                postMessage();
+            });
+            async function postMessage() {
+                const username = formUsernameAsk.value;
+                const message = formMessageAsk.value;
+                const data = {
+                    pseudo: username,
+                    message: message
+                };
+            
+                const postMethod = {
+                    method: 'POST',
+                    body: JSON.stringify(data)
+                };
+            
+                fetch(linkMsgApi, postMethod)
+                    .then(response => response.json())
+                    .then(datas => {
+                        displayMessage(datas)
+                    });
+            }
+
 
     })
 }
 
+
 loadMessages()
 
 
-const postMethod = {
-    method: 'POST',
-    body: JSON.stringify(data)
+
+function displayMessage(message) {
+    let containerHtml = document.getElementById('container')
+    
+    let contentArticle = document.createElement('article');
+    contentArticle.style.marginLeft = '10px';
+    contentArticle.style.alignSelf = 'flex-end'
+
+    let paraPseudo = document.createElement('p');
+    paraPseudo.innerHTML = `${message.pseudo} &agrave; ${message.date}`;
+    paraPseudo.style.margin = '0px';
+    paraPseudo.style.marginTop = '8px';
+    paraPseudo.style.color = 'darkgrey';
+    paraPseudo.style.fontSize = '10px';
+
+    let paraMsgDiv = document.createElement('div');
+    paraMsgDiv.style.backgroundColor = '#00BFFF';
+    paraMsgDiv.style.borderRadius = '20px';
+    paraMsgDiv.style.width = 'fit-content';
+    paraMsgDiv.style.padding = '5px';
+    paraMsgDiv.style.paddingRight = '10px';
+    paraMsgDiv.style.paddingLeft = '10px';
+    paraMsgDiv.style.marginTop = '5px';
+    paraMsgDiv.style.marginRight = '10px';
+    paraMsgDiv.style.marginBottom = '8px';
+
+    let paraMessage = document.createElement('p');
+    paraMessage.innerHTML = message.message;
+    paraMessage.style.margin = '0px';
+
+    paraMsgDiv.appendChild(paraMessage);
+
+    contentArticle.append(paraPseudo, paraMsgDiv);
+    containerHtml.insertBefore(contentArticle, containerHtml.firstChild);
 }
 
-async function postMessage(){
 
 
-    fetch(linkMsgApi, postMethod)
-        .then(response => response.json())
-        .then(datas => {
-
-
-
-        })
-}
